9e980f98b031d047ebd3870241eed419f1ebb373
hommat <zmateusz1111@gmail.com>
2020-01-05 12:44:21 +0100

    Change number rounding before saving to DB

--- functions/utils/index.js
+++ functions/utils/index.js
@@ -1,6 +1,6 @@
 module.exports.prepareNumberToDBsave = function(num) {
   num = typeof val === "string" ? parseFloat(num) : num;
-  num = Math.round(num * 100) / 100;
+  num = Math.round(num * 10) / 10;
 
   return num;
 };
